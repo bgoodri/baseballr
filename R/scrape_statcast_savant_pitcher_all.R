ecf578db45fdc5a97fb64cc962001f4cd3af4b23
Ben Goodrich <goodrich.ben@gmail.com>
2017-08-15 01:25:17 -0400

    pass two non-default arguments to utils::read.csv
    passing na.strings = "null" results in such values being coded as NA
    
    passing stringsAsFactors = FALSE prevents character vectors from being coerced to factors
    
    as a result, most of the subsequent formatting code at the end of these functions is unnecessary and deleted

--- R/scrape_statcast_savant_pitcher_all.R
+++ R/scrape_statcast_savant_pitcher_all.R
@@ -42,7 +42,7 @@ scrape_statcast_savant_pitcher_all <- function(start_date, end_date) {
     {
       print("These data are from BaseballSevant and are property of MLB Advanced Media, L.P. All rights reserved.")
       print("Grabbing data, this may take a minute...")
-      payload <- utils::read.csv(url)
+      payload <- utils::read.csv(url, na.strings = "null", stringsAsFactors = FALSE)
 
     },
     error=function(cond) {
@@ -59,22 +59,8 @@ scrape_statcast_savant_pitcher_all <- function(start_date, end_date) {
     }
   )
   # Clean up formatting.
-  payload[payload=="null"] <- NA
   payload$game_date <- as.Date(payload$game_date, "%Y-%m-%d")
-  payload$des <- as.character(payload$des)
-  payload$game_pk <- as.character(payload$game_pk) %>% as.numeric()
-  payload$on_1b <- as.character(payload$on_1b) %>% as.numeric()
-  payload$on_2b <- as.character(payload$on_2b) %>% as.numeric()
-  payload$on_3b <- as.character(payload$on_3b) %>% as.numeric()
-  payload$release_pos_x <- as.character(payload$release_pos_x) %>% as.numeric()
-  payload$release_pos_x <- as.character(payload$release_pos_x) %>% as.numeric()
-  payload$hit_distance_sc <- as.character(payload$hit_distance_sc) %>% as.numeric()
-  payload$launch_speed <- as.character(payload$launch_speed) %>% as.numeric()
-  payload$launch_angle <- as.character(payload$launch_angle) %>% as.numeric()
-  payload$effective_speed <- as.character(payload$effective_speed) %>% as.numeric()
-  payload$release_spin_rate <- as.character(payload$release_spin_rate) %>% as.numeric()
-  payload$release_extension <- as.character(payload$release_extension) %>% as.numeric()
-  payload$barrel <- with(payload, ifelse(launch_angle <= 50 & launch_speed >= 98 & launch_speed * 1.5 - launch_angle >= 11 & launch_speed + launch_angle >= 124, 1, 0))
+  payload$barrel <- with(payload, ifelse(launch_angle <= 50 & launch_speed >= 98 & launch_speed * 1.5 - launch_angle >= 11 & launch_speed + launch_angle >= 124, 1L, 0L))
   message("URL read and payload aquired successfully.")
 
   return(payload)
